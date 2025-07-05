a0e018a4c25bc3754e41db9b5909e3824b0d6fdc
Carlos-Meneses1 <carlosocharly1@hotmail.com>
2025-07-05 03:53:14 +0000

    excuse-generating project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector('#button').addEventListener("click", () =>{
+  document.querySelector('#the-excuse').innerHTML = generateExcuse();
+  })
+  console.log('Hello, this is an excuse generator.');
+};
+
+let generateExcuse = () => {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let whoIdx = Math.floor(Math.random() * who.length);
+  let actionIdx = Math.floor(Math.random() * action.length);
+  let whatIdx = Math.floor(Math.random() * what.length);
+  let whenIdx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIdx] + ' ' +
+    action[actionIdx] + ' ' +
+    what[whatIdx] + ' ' +
+    when[whenIdx] + '.'
+  );
 };
